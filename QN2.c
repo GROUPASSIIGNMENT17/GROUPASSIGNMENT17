a7fcbb853e7d7cb329f65c5ddcf9048f96e6136c
unknown <mtengainevensia@gmail.com>
2022-06-30 08:28:08 -0700

    EVENSIA MTENGA

---
+++ QN2.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+
+int array(int *p,int *q);
+int main(){
+int *p,*q,i;
+int A[10],B[10];
+array(&A[i],&B[i]);
+
+
+return 0;
+}
+
+int array(int *p,int *q){
+	int A[10]={1,2,3,4,5,6,7,8,9,0};
+	int B[10]={1,5,3,4,5,6,7,8,9,1};
+	int i;	
+p=&A[i];
+q=&B[i];
+
+for (i=0; i<10; i++){
+	if(p[i]==q[i]){
+	printf("true\n");
+	}
+	else{ 
+	printf("false\n");}
+}
+
+ 
+	
+	}
+		
---
+++ QN5.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+int main(){
+	int i, average, maximum, minimum, s_deviation, sum=0, mean;
+      int array[5] = {4, 2, 5, 8, 1};
+        //apply for loop
+            for(i=0; i<5; i++){
+          	//To find the sum
+		    sum = sum + array[i];
+		    //To find the average after gets the sum above
+		    average=sum/5;
+		    //Tofind the smallest and maxmum number
+		    array[0] = maximum;
+		    array[0] = minimum;
+		    if(array[i] > maximum){
+		    maximum = array[i];
+			}
+			else if(array[i] < minimum){
+			array[i] = minimum;	
+			}
+			//Befor calculating sdt deviation should be find the mean
+			mean = sum/5;
+		    }
+			// standard deviation solution,
+			for(i=0; i<5; i++){
+		    sum+= (array[i] - mean) * (array[i] - mean);
+	    	s_deviation = sqrt(sum/5);
+	        }
+		   //GETS THE ANSWERS
+		printf("\n\nTHE AVERAGE IS: %4d ", average);
+	  printf("\n\nTHE MAXIMUM NUMBER IS: %4d ", maximum);
+	printf("\n\nTHE SMALLEST(MINIMUM) NUMBER IS: %4d ", minimum);   
+   printf("\n\nTHE STANDARD DEVIASION IS: %4d ", s_deviation);   
+ return 0;
+}
